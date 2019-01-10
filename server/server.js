896d58c92d8c1e0d3bd6828f0a8195e602ae6659
yogesh kumar <yogeshkumar87@hotmail.com>
2019-01-10 18:49:42 +0530

    init commit

---
+++ server/server.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
+
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then(
+        doc => {
+            res.send(doc);
+        },
+        err => {
+            res.status(400).send(err);
+        }
+    );
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then(
+        todos => {
+            res.send({ todos });
+        },
+        err => {
+            res.status(400).send(err);
+        }
+    );
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then(
+        todo => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+
+            return res.send({ todo });
+        },
+        err => {
+            return res.status(400).send(err);
+        }
+    );
+});
+
+app.listen(port, () => {
+    console.log(`App listening on ${port}`);
+});
