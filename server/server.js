81c4e2df626e8df6e840bd334ba75f528b8d11bc
yogesh kumar <yogeshkumar87@hotmail.com>
2019-01-13 16:59:47 +0530

    update todo added

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var { ObjectID } = require('mongodb');
+var _ = require('lodash');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -56,6 +57,56 @@ app.get('/todos/:id', (req, res) => {
             return res.status(400).send(err);
         }
     );
+});
+
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndDelete(id).then(
+        todo => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+
+            res.send(todo);
+        },
+        err => {
+            res.status(400).send(err);
+        }
+    );
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+        .then(todo => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+
+            res.send(todo);
+        })
+        .catch(err => {
+            res.status(400).send();
+        });
 });
 
 app.listen(port, () => {
